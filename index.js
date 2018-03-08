78c0f067640809ad5a213db6265420321665b4e7
Alan-Miller <alan.ashman@gmail.com>
2018-03-08 02:56:07 -0700

    ghs files pushed to new repo

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+const argsOptions = require('./lib/options/args');
+const showArgs = require('./lib/showArgs');
+const showRepos = require('./lib/showRepos');
+const showUser = require('./lib/showUser');
+const makeRepo = require('./lib/makeRepo');
+
+const yargs = require('yargs')
+  .options({
+    'args': argsOptions
+  })
+  // .command(['args'], 'See all args', yargs => { }, args => { showArgs(yargs) })
+  .command(['make'], 'Make repo', yargs => { }, args => { makeRepo(yargs) })
+  .command(['show'], 'Show public repos', yargs => { }, args => { showRepos(yargs) })
+  .command(['user'], 'Show user info', yargs => { }, args => { showUser(yargs) })
+
+const args = yargs.argv;
+
+if (args.args) console.log('args', args);
+
+console.log('YARG!');
